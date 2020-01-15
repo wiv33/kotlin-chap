a7cfca25a87af03503c3a3140ef1325f2f5ff68d
PSK <wiv821253@gmail.com>
2020-01-16 00:27:34 +0900

    init configServer

---
+++ chapter6/configserver/src/main/kotlin/com/psawesome/configserver/ConfigserverApplication.kt
@@ -0,0 +1,14 @@
+package com.psawesome.configserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConfigserverApplication>(*args)
+}
