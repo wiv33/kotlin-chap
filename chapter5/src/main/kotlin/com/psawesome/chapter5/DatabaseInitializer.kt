9e6df58ef2ea65a6c93cef8204d8040f801e0c66
PSK <wiv821253@gmail.com>
2020-01-15 21:37:45 +0900

    save default customer

--- chapter5/src/main/kotlin/com/psawesome/chapter5/DatabaseInitializer.kt
+++ chapter5/src/main/kotlin/com/psawesome/chapter5/DatabaseInitializer.kt
@@ -1,5 +1,6 @@
 package com.psawesome.chapter5
 
+import com.psawesome.chapter5.Customer.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.stereotype.Component
@@ -14,7 +15,17 @@ DATE: 2020-01-15 수요일 20:58
 class DatabaseInitializer {
 
     @Autowired
+    lateinit var customerRepository: CustomerRepository
+
+    @Autowired
     lateinit var mongoOperations: ReactiveMongoOperations
+
+    companion object {
+        val initialCustomers = listOf(
+                Customer(1, "kotlin"),
+                Customer(2, "Flux"),
+                Customer(3, "Mono", Telephone("+82", "102572934")))
+    }
 
     @PostConstruct
     fun initData() {
@@ -23,6 +34,9 @@ class DatabaseInitializer {
                 mongoOperations.createCollection("Customers").subscribe {
                     println("Customers collections created")
                 } else println("Customers collections already exist")
+            customerRepository.saveAll(initialCustomers).subscribe{
+                println("Default customers created")
+            }
         }
     }
 }
